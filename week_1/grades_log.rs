3944e604548a368b2015dc928388a19a555fe14c
basil <basil.lampr@gmail.com>
2023-09-03 17:47:23 +0300

    almost finished courses predetermination

--- week_1/grades_log.rs
+++ week_1/grades_log.rs
@@ -7,34 +7,41 @@ fn user_input_str() -> String {
     return user_input
 }
 
+fn duplicate_control(course: &String, courses: &Vec<String>) -> bool {
+    for existing_course in courses.iter() {
+        if existing_course.to_lowercase() == course.to_lowercase() {
+            return true;
+        }
+    }
+    return false;
+}
+
 fn add_course(courses: &mut Vec<String>) {
     loop {
-        // TODO implement the user_input_str method 
-
         println!("Please enter course title");
         let mut course_title = String::new();
         io::stdin().read_line(&mut course_title).expect("Failed to read line");
-        let course_title = course_title.trim();  // Remove any trailing whitespace
+        let course_title = course_title.trim();
 
-        // Input validation
-        if course_title.is_empty() || course_title.parse::<f64>().is_ok() {
-            println!("Invalid course name. Please enter a valid course name.");
+        if duplicate_control(&course_title.to_string(), courses) {
+            println!("This course already exists. Do you want to enter another course? [Yy/Nn]");
+            //TODO circular logic to confirm whether user wants to add another course
         } else {
-            courses.push(course_title.to_string());
-            println!("You have added the course: {}", course_title);
-            break;
+            if course_title.is_empty() || course_title.parse::<f64>().is_ok() {
+                println!("Invalid course name. Please enter a valid course name.");
+            } else {
+                courses.push(course_title.to_string());
+                println!("You have added the course: {}", course_title);
+                break;
+            }
         }
     }
 }
 
 fn course_list(courses: &mut Vec<String>) {
-    loop { 
+    loop {
+        println!("Do you want to add another course? [Yy/Nn]");
 
-        // TODO implement the user_input_str method 
-        let mut user_input = String::new();
-
-        io::stdin().read_line(&mut user_input).expect("Failed to read line");
-        // TODO implement the user_input_str method 
         let mut user_input = String::new();
 
         io::stdin().read_line(&mut user_input).expect("Failed to read line");
@@ -42,9 +49,8 @@ fn course_list(courses: &mut Vec<String>) {
 
         if user_input == "Y" || user_input == "y" || user_input == "Yy" {
             add_course(courses); 
-            println!("Do you want to add additional courses? (Yy/Nn)");
         } else {
-            println!("No additional courses will be added.\n\n\n");
+            println!("No courses will be added.\n\n\n");
             break;
         }
     }
@@ -57,7 +63,12 @@ fn course_list(courses: &mut Vec<String>) {
 
 fn course_finalize(courses: &mut Vec<String>) {
     loop {
-        println!("Is this list correct? [Yy/Nn]");
+        let c = courses.iter()
+                       .map(|course| format!("    - {}", course))
+                       .collect::<Vec<String>>()
+                       .join("\n");
+        println!("Is this list correct? [Yy/Nn]\n{}", c);
+        
         let mut user_input = user_input_str();
 
         if user_input == "y" {
@@ -67,14 +78,9 @@ fn course_finalize(courses: &mut Vec<String>) {
             user_input = user_input_str();
 
             if user_input == "add" {
-                course_list(courses);
+                add_course(courses);
             } else if user_input == "remove" {
                 println!("Which course do you want to remove?");
-                let course_list = courses.iter()
-                                        .map(|course| format!("    - {}", course))
-                                        .collect::<Vec<String>>()
-                                        .join("\n");
-                println!("Courses currently offered are: {}", course_list);
                 let course_to_remove = user_input_str();
                 if let Some(pos) = courses.iter().position(|x| x.to_lowercase() == course_to_remove) { // if course_to_remove in list of courses 
                     courses.remove(pos);
@@ -93,8 +99,7 @@ fn main() {
 
     // Welcome message start
     println!("Welcome to Student Management System 0.0.0.1 - 
-    The base courses are \n    - {}, \n    - {}, \n    - {}.\n\n
-    Do you want to add additional courses? (Yy/Nn)",
+    The base courses are \n    - {}, \n    - {}, \n    - {}.\n\n",
     courses[0], courses[1], courses[2]);
     // Welcome message end
 
